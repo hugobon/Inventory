bb8c2352a246f149b64d10c11117bd941986753c
Zulhilmi <zulhilmi.aw@e-oasia.com>
2018-02-08 01:04:29 +0800

    20180207/Zul/SupplierModule
    

--- resources/views/Supplier/supplier.blade.php
+++ resources/views/Supplier/supplier.blade.php
@@ -5,33 +5,104 @@
 
 <!-- START BREADCRUMB -->
 <ul class="breadcrumb">
-    <li><a href="#">Link</a></li>                    
-    <li class="active">Active</li>
+    <li><a href="{{ url('home') }}">Home</a></li>
+    <li><a href="#">Supplier</a></li>
+    <li class="active">Supplier Detail</li>
 </ul>
-<!-- END BREADCRUMB -->                
-
-<div class="page-title">                    
-    <h2><span class="fa fa-arrow-circle-o-left"></span> Page Title</h2>
-</div>                   
+<!-- END BREADCRUMB -->                  
 
 <!-- PAGE CONTENT WRAPPER -->
 <div class="page-content-wrap">
-
     <div class="row">
         <div class="col-md-12">
-
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Panel Title</h3>
+            <form class="form-horizontal">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Supplier Detail</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Company Name</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Address 1</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Address 2</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Postal Code</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">City</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Region</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Country</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Telephone</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Fax No</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Attn no</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Email</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        <button class="btn btn-default">Clear Form</button>                                    
+                        <button class="btn btn-primary pull-right">Submit</button>
+                    </div>
                 </div>
-                <div class="panel-body">
-                    Panel body
-                </div>
-            </div>
-
+            </form>
         </div>
     </div>
-
 </div>
 <!-- END PAGE CONTENT WRAPPER -->
 
